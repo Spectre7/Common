9fe26fb64a25d2440ae507c36bd31d403fb9c959
giuseppe2048 <bauo81@gmail.com>
2016-12-13 10:58:02 +0100

    array

---
+++ src/main/java/corsojavagenesis/common/arrays/SintassiArray.java
@@ -0,0 +1,33 @@
+package corsojavagenesis.common.arrays;
+
+public class SintassiArray {
+
+	public static void main(String[] args) {
+		int[] b;
+		int[] c = new int[4]; 
+		
+		b = new int[10];
+		
+		
+		c[0]=1;
+		c[1]=2;
+		c[2]=3;
+		c[3]=4;
+		
+		
+		for (int i=0;i<4;i++){
+			c[i] = i+1;
+		}
+		
+		
+		System.out.println("c[2] = " + c[2]);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
